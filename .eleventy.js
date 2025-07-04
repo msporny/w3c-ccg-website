9610f1367c0af5bb6f71a33f4308a0f0dfa5c3a0
Manu Sporny <msporny@digitalbazaar.com>
2025-07-04 18:38:19 -0400

    Add initial 11ty setup for website.

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function(eleventyConfig) {
+  // Add navigation plugin
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  // Copy static assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+
+  // Watch for changes in CSS and JS files
+  eleventyConfig.addWatchTarget("src/css/");
+  eleventyConfig.addWatchTarget("src/js/");
+
+  // Set custom directories
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
+};
